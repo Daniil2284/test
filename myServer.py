b4c9893914add2b1dd3626e814e4c5479d880fb7
Daniil2284 <58908291+Daniil2284@users.noreply.github.com>
2026-03-04 10:55:39 +0300

    edit file

--- myServer.py
+++ myServer.py
@@ -2,10 +2,11 @@
 import random 
 
 users = "root"
-age = random.randint(10,25)
+
 print("Hello")
 def main():
     while True:
+        age = random.randint(10,25)
         if age == 18 and users == "root":
             print("Доступ на сервер безумные зомби разрешен")
             print("Админа не оскорблять")
