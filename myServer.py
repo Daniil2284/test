f577605cd6886e815dc28e70304f9979592934a2
Daniil2284 <58908291+Daniil2284@users.noreply.github.com>
2026-03-04 10:07:43 +0300

    final variable

--- myServer.py
+++ myServer.py
@@ -4,10 +4,12 @@ users = "root"
 age = random.randint(10,25)
 
 def main():
-    if age == 18 and users == "root":
-        print("Доступ на сервер безумные зомби разрешен")
-    else:
-        print("Вход запрещен!")
+    while True:
+        if age == 18 and users == "root":
+            print("Доступ на сервер безумные зомби разрешен")
+            break
+        else:
+            print("Вход запрещен!")
 
 if __name__ == "__main__":
     main()
